be575942cdb51b7cf87280a6f3fbcd9f070216a5
ingramwd <Ingramwd01@gmail.com>
2022-03-24 22:00:27 -0400

    added mutations and fixed up user profiles and home.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Redirect, useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
 import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import FriendList from '../components/FriendLists';
+import Auth from '../utils/auth';
 
 const Profile = () => {
-  const { username: userParams } = useParams();
+  const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(QUERY_USER, {
-    variables: { username: userParams }
+  //redirect to personal profile page if username is the logged-in user's
+
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam }
   });
 
-  const user = data?.user || {};
+  const user = data?.me || data?.user || {};
 
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Redirect to="/profile" />;
+  }
   if (loading) {
     return <div>loading...</div>
   }
 
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+      </h4>
+    );
+  }
+
   return (
     <div>
+      <h2 className='bg-dark text-secondary p-3 display-inline-block'>
+        Viewing {userParam ? `${user.username}'s` : "your"} profile.
+      </h2>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           {user.username}
@@ -34,7 +51,7 @@ const Profile = () => {
           <FriendList
             username={user.username}
             friendCount={user.friendCount}
-            friends={user.frineds}
+            friends={user.friends}
           />
         </div>
       </div>
